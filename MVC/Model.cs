cfd54dc6f2649db31555fea8565fde2dcbe0ff99
Drenbb <Aa20021337>
2022-01-27 16:02:20 +0300

    Добавьте файлы проекта.

---
+++ MVC/Model.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MVC
+{
+    static class Model
+    {
+        //Данные 
+        static int n = 0;
+        public static List<string> fam = new List<string>() { "Иванов", "Петров", "Сидоров"};
+        //Интерфейс
+        public static TextBlock tbN;
+        public static TextBlock tbList;
+        //БЛ
+        public static int N 
+        { 
+            get 
+            { 
+                return n; 
+            }
+            set
+            {
+                n = value;
+                tbN.Text = n.ToString();
+            }
+        }
+
+        public static int Ind 
+        {
+            set 
+            { 
+                tbList.Text = fam[value];
+            }
+
+        }
+    }
+}
